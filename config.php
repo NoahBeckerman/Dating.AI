3ed16ad43b477077a54abc44b2ed69c8b8b2a634
Your Name <you@example.com>
2023-10-22 00:27:48 -0400

    Update db name.

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'your_database_host');
 define('DB_USER', 'your_database_username');
 define('DB_PASS', 'your_database_password');
-define('DB_NAME', 'your_database_name');
+define('DB_NAME', 'DatingAI');
 
 define('OPENAI_API_KEY', 'api_key');
 define('ENGINE_NAME', 'YOUR_ENGINE_NAME');
