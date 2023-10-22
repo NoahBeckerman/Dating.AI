587d2cf8046f5f6aee7b134b290b4c494ad97894
Your Name <you@example.com>
2023-10-22 01:44:13 -0400

    Updated db config with test creds.

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
 // Database credentials
-define('DB_HOST', 'your_database_host');
-define('DB_USER', 'your_database_username');
-define('DB_PASS', 'your_database_password');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'BHS ');
+define('DB_PASS', 'BHS');
 define('DB_NAME', 'DatingAI');
 
 define('OPENAI_API_KEY', 'api_key');
 define('ENGINE_NAME', 'YOUR_ENGINE_NAME');
-define('MAX_TOKES', 4096)
+define('MAX_TOKES', 4096);
 define('TEMPERATURE', 0.7)
+?>
--- database.sql
+++ database.sql
@@ -1,12 +1,13 @@
 -- Create database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS DatingAI;
+CREATE DATABASE IF NOT EXISTS DatingAI CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 
 -- Use the database
 USE DatingAI;
 
 -- Create users table if it doesn't exist
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -20,15 +21,16 @@ CREATE TABLE IF NOT EXISTS users (
     subscription INT,
     role INT,
     banned BOOLEAN,
-    signup_date TIMESTAMP,
-    last_login TIMESTAMP,
+    signup_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_login TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     total_messages_sent INT,
     total_cost_of_queries FLOAT
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 -- Create personalities table if it doesn't exist
 CREATE TABLE IF NOT EXISTS personalities (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     profile_picture VARCHAR(255),
@@ -39,24 +41,25 @@ CREATE TABLE IF NOT EXISTS personalities (
     sex VARCHAR(10),
     location VARCHAR(50),
     pre_prompt TEXT
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 -- Create chat history table if it doesn't exist
 CREATE TABLE IF NOT EXISTS chat_history (
     id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    user_id INT(11) UNSIGNED NOT NULL,
-    personality_id INT(11) UNSIGNED NOT NULL,
+    user_id INT(11) UNSIGNED,
+    personality_id INT(11) UNSIGNED,
     message TEXT NOT NULL,
     response TEXT NOT NULL,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (personality_id) REFERENCES personalities(id)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Test data for users table
 INSERT INTO users (email, username, password, addr1, addr2, zip, state, country, first_name, last_name, subscription, role, banned, signup_date, last_login, total_messages_sent, total_cost_of_queries) VALUES
-('john.doe@example.com', 'JohnDoe', 'password123', '123 Main St', 'Apt 4', '12345', 'NY', 'USA', 'John', 'Doe', 1, 0, FALSE, NOW(), NOW(), 10, 5.00),
-('jane.doe@example.com', 'JaneDoe', 'password123', '456 Elm St', 'Suite 789', '67890', 'CA', 'USA', 'Jane', 'Doe', 2, 1, FALSE, NOW(), NOW(), 20, 10.00);
+('john.doe@example.com', 'JohnDoe', 'password123', '123 Main St', 'Apt 4', '12345', 'NY', 'USA', 'John', 'Doe', 1, 0, FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 10, 5.00),
+('jane.doe@example.com', 'JaneDoe', 'password123', '456 Elm St', 'Suite 789', '67890', 'CA', 'USA', 'Jane', 'Doe', 2, 1, FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 20, 10.00);
 
 -- Test data for personalities table
 INSERT INTO personalities (first_name, last_name, profile_picture, description, notes, likes, dislikes, sex, location, pre_prompt) VALUES
@@ -65,9 +68,10 @@ INSERT INTO personalities (first_name, last_name, profile_picture, description, 
 
 -- Test data for chat_history table
 INSERT INTO chat_history (user_id, personality_id, message, response, timestamp) VALUES
-(1, 1, 'Hello John!', 'Hi there! How are you?', NOW()),
-(2, 2, 'Hi Jane.', 'Hello! How can I assist you today?', NOW());
+(1, 1, 'Hello John!', 'Hi there! How are you?', CURRENT_TIMESTAMP),
+(2, 2, 'Hi Jane.', 'Hello! How can I assist you today?', CURRENT_TIMESTAMP);
 
 -- Role and Subscription Classification:
 -- Roles: 0 = New User, 1 = Moderator, 2 = Admin, 3 = Superadmin, 100 = Owner.
 -- Subscriptions: 1 = Tier One Subscription, 2 = Tier Two Subscription, 3 = Tier Three Subscription (VIP).
+
