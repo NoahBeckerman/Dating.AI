ebfc66c6b47351e2fbac8ca466b8ef1674b4d855
Your Name <you@example.com>
2023-11-04 02:56:57 -0400

    Profile Picture works.

--- profile.php
+++ profile.php
@@ -16,6 +16,23 @@ $userId = $_SESSION["user_id"];
 // Fetch user details
 $user = getUserById($userId);
 
+$folderName = md5($user["username"]);
+$pathToFolder = "USER_DATA/" . $folderName;
+
+$mostRecentPic = null;
+if (is_dir($pathToFolder)) {
+    $files = scandir($pathToFolder);
+    rsort($files); // Sort files in descending order
+
+    foreach ($files as $file) {
+        if (strpos($file, ".") !== 0) {
+            // Check if file is not a directory (like '.' or '..')
+            $mostRecentPic = $pathToFolder . "/" . $file;
+            break;
+        }
+    }
+}
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Clear chat history
     if (isset($_POST["clear_chat_history"])) {
@@ -38,90 +55,124 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         }
     }
 
-   // If update_account button was clicked
-   if (isset($_POST["update_account"])) {
-    $fieldsToUpdate = [];
-    $params = [];
+    // If update_account button was clicked
+    if (isset($_POST["update_account"])) {
+        $fieldsToUpdate = [];
+        $params = [];
 
-    // Check each field for changes
-    foreach (['username', 'email', 'password', 'age', 'preferences'] as $field) {
-        if ($_POST[$field] !== $user[$field]) {
-            $fieldsToUpdate[] = "$field = ?";
-            $params[] = $_POST[$field];
+        // Check each field for changes
+        foreach (
+            ["username", "email", "password", "age", "preferences"]
+            as $field
+        ) {
+            if ($_POST[$field] !== $user[$field]) {
+                if ($field === "password") {
+                    // Hash the password before updating
+                    $hashedPassword = password_hash(
+                        $_POST[$field],
+                        PASSWORD_DEFAULT
+                    );
+                    $fieldsToUpdate[] = "$field = ?";
+                    $params[] = $hashedPassword;
+                } else {
+                    $fieldsToUpdate[] = "$field = ?";
+                    $params[] = $_POST[$field];
+                }
+            }
+        }
+
+        // If there are fields to update
+        if (!empty($fieldsToUpdate)) {
+            $query =
+                "UPDATE users SET " .
+                implode(", ", $fieldsToUpdate) .
+                " WHERE id = ?";
+            $params[] = $userId;
+
+            executeNonQuery($query, $params);
+            // Set a success message to be displayed in modal.php
+            SystemFlag(
+                "UpdateAccountSuccess",
+                "Change complete.",
+                "SUCCESS",
+                1
+            );
         }
     }
 
-    // If there are fields to update
-    if (!empty($fieldsToUpdate)) {
-        $query = "UPDATE users SET " . implode(", ", $fieldsToUpdate) . " WHERE id = ?";
-        $params[] = $userId;
+    // If update_billing button was clicked
+    if (isset($_POST["update_billing"])) {
+        $fieldsToUpdate = [];
+        $params = [];
 
-        executeNonQuery($query, $params);
-       // Set a success message to be displayed in modal.php
-    SystemFlag(
-      "UpdateAccountSuccess",
-      "Change complete.",
-      "SUCCESS",
-      1
-    );
-  
-  
-    }
-}
+        // Check each field for changes
+        foreach (
+            [
+                "addr1",
+                "addr2",
+                "zip",
+                "state",
+                "country",
+                "first_name",
+                "last_name",
+            ]
+            as $field
+        ) {
+            if ($_POST[$field] !== $user[$field]) {
+                $fieldsToUpdate[] = "$field = ?";
+                $params[] = $_POST[$field];
+            }
+        }
 
-// If update_billing button was clicked
-if (isset($_POST["update_billing"])) {
-    $fieldsToUpdate = [];
-    $params = [];
+        // If there are fields to update
+        if (!empty($fieldsToUpdate)) {
+            $query =
+                "UPDATE users SET " .
+                implode(", ", $fieldsToUpdate) .
+                " WHERE id = ?";
+            $params[] = $userId;
 
-    // Check each field for changes
-    foreach (['addr1', 'addr2', 'zip', 'state', 'country', 'first_name', 'last_name'] as $field) {
-        if ($_POST[$field] !== $user[$field]) {
-            $fieldsToUpdate[] = "$field = ?";
-            $params[] = $_POST[$field];
+            executeNonQuery($query, $params);
+
+            // Set a success message to be displayed in modal.php
+            SystemFlag(
+                "UpdateBillingSuccess",
+                "Change complete.",
+                "SUCCESS",
+                1
+            );
         }
     }
 
-    // If there are fields to update
-    if (!empty($fieldsToUpdate)) {
-        $query = "UPDATE users SET " . implode(", ", $fieldsToUpdate) . " WHERE id = ?";
-        $params[] = $userId;
+    if (
+        isset($_FILES["profile_picture"]) &&
+        $_FILES["profile_picture"]["error"] == 0
+    ) {
+        $username = $_POST["username"];
+        $hashedFolderName = md5($username);
+        $targetFolder = "USER_DATA/" . $hashedFolderName;
 
+        if (!file_exists($targetFolder)) {
+            mkdir($targetFolder, 0777, true);
+        }
+
+        $timestamp = time();
+        $fileExtension = pathinfo(
+            $_FILES["profile_picture"]["name"],
+            PATHINFO_EXTENSION
+        );
+        $newFileName = $timestamp . "." . $fileExtension;
+
+        move_uploaded_file(
+            $_FILES["profile_picture"]["tmp_name"],
+            $targetFolder . "/" . $newFileName
+        );
+
+        // Update the 'profile_picture' column in the database
+        $query = "UPDATE users SET profile_picture = ? WHERE id = ?";
+        $params = [$newFileName, $userId];
         executeNonQuery($query, $params);
-        
-           // Set a success message to be displayed in modal.php
-    SystemFlag(
-      "UpdateBillingSuccess",
-      "Change complete.",
-      "SUCCESS",
-      1
-  );
-
     }
-}
-
-if (isset($_FILES['profile_picture']) && $_FILES['profile_picture']['error'] == 0) {
-  $username = $_POST['username'];
-  $hashedFolderName = md5($username);
-  $targetFolder = "USER_DATA/" . $hashedFolderName;
-  
-  if (!file_exists($targetFolder)) {
-      mkdir($targetFolder, 0777, true);
-  }
-  
-  $timestamp = time();
-  $fileExtension = pathinfo($_FILES['profile_picture']['name'], PATHINFO_EXTENSION);
-  $newFileName = $timestamp . "." . $fileExtension;
-  
-  move_uploaded_file($_FILES['profile_picture']['tmp_name'], $targetFolder . "/" . $newFileName);
-  
-  // Update the 'profile_picture' column in the database
-  $query = "UPDATE users SET profile_picture = ? WHERE id = ?";
-  $params = [$newFileName, $userId];
-  executeNonQuery($query, $params);
-}
-
-
 }
 ?>
 <!DOCTYPE html>
@@ -143,15 +194,30 @@ if (isset($_FILES['profile_picture']) && $_FILES['profile_picture']['error'] == 
           <div id="collapseOne" class="collapse show" data-parent="#accordion">
             <div class="card-body">
               <form action="profile.php" method="post" enctype="multipart/form-data" class="form-signin">
-                 Username <input type="text" name="username" class="form-control" placeholder="Username" value="<?php echo htmlspecialchars($user['username']); ?>" required> 
-                 Email <input type="email" name="email" class="form-control" placeholder="Email" value="<?php echo htmlspecialchars($user['email']); ?>"required> 
-                 Password <input type="password" name="password" class="form-control" placeholder="Password" required>
-                 Age <input type="text" name="age" class="form-control" placeholder="" value="<?php echo htmlspecialchars($user['age']); ?>" > 
-                 Preferences <input type="text" name="preferences" class="form-control" placeholder="" value="<?php echo htmlspecialchars($user['preferences']); ?>"> 
+                 Username <input type="text" name="username" class="form-control" placeholder="Username" value="<?php echo htmlspecialchars(
+                     $user["username"]
+                 ); ?>"> 
+                 Email <input type="email" name="email" class="form-control" placeholder="Email" value="<?php echo htmlspecialchars(
+                     $user["email"]
+                 ); ?>"> 
+                 Password <input type="password" name="password" class="form-control" placeholder="Password">
+                 Age <input type="text" name="age" class="form-control" placeholder="" value="<?php echo htmlspecialchars(
+                     $user["age"]
+                 ); ?>" > 
+                 Preferences <input type="text" name="preferences" class="form-control" placeholder="" value="<?php echo htmlspecialchars(
+                     $user["preferences"]
+                 ); ?>"> 
                   Profile Picture <div class="custom-file">
-                  <input type="file" class="custom-file-input" id="profile_picture" name="profile_picture">
-                  <label class="custom-file-label" for="profile_picture">Choose Profile Picture</label>
+                  <?php if ($mostRecentPic): ?>
+                 <div class="mb-3">
+                    <label>Current Profile Picture:</label>
+                    <img src="<?php echo $mostRecentPic; ?>" alt="Current Profile Picture" width="100">
+                  </div>
+                  <?php endif; ?>
+                  <input type="file" name="profile_picture" id="profile_picture">                 
                 </div>
+                <br>
+                <br>
                 <button type="submit" name="update_account" class="btn btn-primary">Update Account</button>
               </form>
             </div>
@@ -165,13 +231,27 @@ if (isset($_FILES['profile_picture']) && $_FILES['profile_picture']['error'] == 
           <div id="collapseTwo" class="collapse" data-parent="#accordion">
             <div class="card-body">
               <form action="profile.php" method="post" class="form-signin"> 
-                Address Line 1 <input type="text" name="addr1" class="form-control" placeholder="Address Line 1" value="<?php echo htmlspecialchars($user['addr1']); ?>">
-               Address Line 2 <input type="text" name="addr2" class="form-control" placeholder="Address Line 2" value="<?php echo htmlspecialchars($user['addr2']); ?>"> 
-               ZIP Code <input type="text" name="zip" class="form-control" placeholder="ZIP Code" value="<?php echo htmlspecialchars($user['zip']); ?>">
-                State <input type="text" name="state" class="form-control" placeholder="State" value="<?php echo htmlspecialchars($user['state']); ?>">
-                 Country <input type="text" name="country" class="form-control" placeholder="Country" value="<?php echo htmlspecialchars($user['country']); ?>"> 
-                 First Name <input type="text" name="first_name" class="form-control" placeholder="First Name" value="<?php echo htmlspecialchars($user['first_name']); ?>"> 
-                 Last Name <input type="text" name="last_name" class="form-control" placeholder="Last Name" value="<?php echo htmlspecialchars($user['last_name']); ?>">
+                Address Line 1 <input type="text" name="addr1" class="form-control" placeholder="Address Line 1" value="<?php echo htmlspecialchars(
+                    $user["addr1"]
+                ); ?>">
+               Address Line 2 <input type="text" name="addr2" class="form-control" placeholder="Address Line 2" value="<?php echo htmlspecialchars(
+                   $user["addr2"]
+               ); ?>"> 
+               ZIP Code <input type="text" name="zip" class="form-control" placeholder="ZIP Code" value="<?php echo htmlspecialchars(
+                   $user["zip"]
+               ); ?>">
+                State <input type="text" name="state" class="form-control" placeholder="State" value="<?php echo htmlspecialchars(
+                    $user["state"]
+                ); ?>">
+                 Country <input type="text" name="country" class="form-control" placeholder="Country" value="<?php echo htmlspecialchars(
+                     $user["country"]
+                 ); ?>"> 
+                 First Name <input type="text" name="first_name" class="form-control" placeholder="First Name" value="<?php echo htmlspecialchars(
+                     $user["first_name"]
+                 ); ?>"> 
+                 Last Name <input type="text" name="last_name" class="form-control" placeholder="Last Name" value="<?php echo htmlspecialchars(
+                     $user["last_name"]
+                 ); ?>">
                 <button type="submit" name="update_billing" class="btn btn-primary">Update Billing</button>
               </form>
             </div>
@@ -216,3 +296,9 @@ if (isset($_FILES['profile_picture']) && $_FILES['profile_picture']['error'] == 
 </body>
 </html>
 
+<script>
+    document.getElementById('profile_picture').addEventListener('change', function() {
+        var fileName = this.files[0].name;
+        document.getElementById('fileLabel').textContent = fileName;
+    });
+</script>
