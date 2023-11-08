737f6c9c922802f1a0e1a366e318606d36962146
Your Name <you@example.com>
2023-11-07 20:34:41 -0500

    added admin section

---
+++ BACKEND/ADMIN/admin_dashboard.php
@@ -0,0 +1,7 @@
+<?php
+
+//This will be the main layout for your admin dashboard.
+//Use the existing CSS files to maintain consistency with the website's look and feel.
+//Include navigation links or buttons to the different sections of the dashboard (Overview, User Management, etc.).
+
+?>
